0b7d2a6e473d58a0a27990714e3f2d0cc5833894
Peter Braun <peter.braun@helmholtz-berlin.de>
2024-12-19 18:35:41 +0100

    removed unneccesary comments

--- lib/secop_client.ex
+++ lib/secop_client.ex
@@ -23,32 +23,5 @@ defmodule SecopClient do
 
     Supervisor.start_link(children, opts)
 
-
-
-    # Buffer.start_link(buffer_id)
-
-    # [{buffer_pid,_value}] = Registry.lookup(Registry.Buffer,buffer_id)
-
-    # Buffer.receive(buffer_pid,"dscpsdcj\n")
-
-    # {:ok, pid} = TcpConnection.start_link(host: ~c"127.0.0.1", port: 10800, reconnect_backoff: 10000)
-
-    # TcpConnection.send(pid,~c"describe .\n")
-    # TcpConnection.send(pid,~c"activate\n")
-
-    #node_id = {~c"192.168.178.52", 10800}
-
-    #SEC_Node_Statem.start_link(host: ~c"127.0.0.1", port: 10800, reconnect_backoff: 1000)
-
-
-    #SEC_Node_Supervisor.start_child(host: ~c"127.0.0.1", port: 10800, reconnect_backoff: 1000)
-
-
-    #Process.sleep(1000)
-
-    #SEC_Node_Statem.change(node_id, "massflow_contr1", "target", 20)
-
-
-    #{:ok, self()}
   end
 end
